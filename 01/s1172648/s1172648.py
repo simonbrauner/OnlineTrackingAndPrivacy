17314873084171dc48b26afa70aa9e014a1dd91c
Šimon Brauner <simonbrauner@seznam.cz>
2025-09-29 20:19:06 +0200

    feat(01): argv

--- 01/s1172648/s1172648.py
+++ 01/s1172648/s1172648.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
-print("TODO")
+from sys import argv, exit
 
+def analyze_har(path):
+    print(f'TODO: {path}')
+
+
+def main():
+    if len(argv) != 2:
+        print(f'Usage: {argv[0]} <HAR path>')
+        exit(1)
+
+    analyze_har(argv[1])
+
+
+if __name__ == '__main__':
+    main()
