991f39203c9a94531a12fdd3b97bca7f3c768a69
Šimon Brauner <simonbrauner@seznam.cz>
2025-09-29 20:40:45 +0200

    feat(01): num_reqs

--- 01/s1172648/s1172648.py
+++ 01/s1172648/s1172648.py
@@ -4,23 +4,42 @@ from sys import argv, exit
 
 from json import loads
 
+
+def num_reqs(log, results):
+    results['num_reqs'] = len(log['entries'])
+
+
+def collect_results(log):
+    results = dict()
+
+    num_reqs(log, results)
+
+    return results
+
+
 def analyze_har(path):
     try:
         with open(path, 'r') as file:
-            contents = file.read()
+            har = file.read()
     except Exception as e:
         print(f'Failed to read file: {e}')
         exit(1)
 
-    har = loads(contents)
-    print(har)
+    log = loads(har)['log']
+    #print(log['entries'][0].keys())
+
+    results = collect_results(log)
+
+    return results
 
 def main():
     if len(argv) != 2:
         print(f'Usage: {argv[0]} <HAR path>')
         exit(1)
 
-    analyze_har(argv[1])
+    results = analyze_har(argv[1])
+
+    print(results)
 
 
 if __name__ == '__main__':
